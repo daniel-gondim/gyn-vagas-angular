3779f70e2213a61652af3705f20a965e4709e312
Daniel <danielgondimdecastro@gmail.com>
2024-03-26 20:59:21 -0300

    feat: commit inicial do componente cadastro-vagas

---
+++ src/app/cadastro-vagas/cadastro-vagas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-vagas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-vagas.component.html',
+  styleUrl: './cadastro-vagas.component.css'
+})
+export class CadastroVagasComponent {
+
+}
