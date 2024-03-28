b7abcb53b4e77f03aa0b8dbb4f0faaed53c138ab
Daniel <danielgondimdecastro@gmail.com>
2024-03-28 20:17:34 -0300

    feat: adiciona imports e propriedades para manipulação de vagas em CadastroVagasComponent

--- src/app/cadastro-vagas/cadastro-vagas.component.ts
+++ src/app/cadastro-vagas/cadastro-vagas.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
+import { Vaga } from '../service/servico-vaga.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-vagas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro-vagas.component.html',
-  styleUrl: './cadastro-vagas.component.css'
+  styleUrl: './cadastro-vagas.component.css',
 })
 export class CadastroVagasComponent {
+  private vagas: Vaga[] = [];
+  nome: any;
+  salario: any;
+  descricao: any;
+  empresa: any;
+  contato: any;
 
-adicionarVaga() {
-throw new Error('Method not implemented.');
-}
+  constructor() {}
 
+  adicionarVaga() {
+    throw new Error('Method not implemented.');
+  }
 }
