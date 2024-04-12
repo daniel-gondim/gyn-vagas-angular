88bf3f49428173177d13f647cd03def65644d457
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-12 15:57:31 -0300

    fix: adiciona componente tabela-vagas aos imports do módulo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CadastroVagasComponent } from "./cadastro-vagas/cadastro-vagas.component";
+import { TabelaVagasComponent } from './tabela-vagas/tabela-vagas.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CadastroVagasComponent]
+    imports: [RouterOutlet, CadastroVagasComponent, TabelaVagasComponent]
 })
 export class AppComponent {
   title = 'gyn-vagas-angular';
