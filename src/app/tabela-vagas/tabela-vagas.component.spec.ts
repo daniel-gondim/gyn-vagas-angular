74b6f5b58b81ee18bf313a0a8ac7fbf8f893df51
Daniel <danielgondimdecastro@gmail.com>
2024-04-12 20:16:11 -0300

    test: corrige o seletor para selecionar a linha correta

--- src/app/tabela-vagas/tabela-vagas.component.spec.ts
+++ src/app/tabela-vagas/tabela-vagas.component.spec.ts
@@ -1,68 +1,3 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { TabelaVagasComponent } from './tabela-vagas.component';
-// import { ServicoVagaService, Vaga } from '../service/servico-vaga.service';
-
-// describe('TabelaVagasComponent', () => {
-//   let component: TabelaVagasComponent;
-//   let fixture: ComponentFixture<TabelaVagasComponent>;
-//   let servicoVagaService: ServicoVagaService;
-
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [],
-//       providers: [ServicoVagaService],
-//     }).compileComponents();
-//   });
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TabelaVagasComponent);
-//     component = fixture.componentInstance;
-//     servicoVagaService = TestBed.inject(ServicoVagaService);
-//     fixture.detectChanges();
-//   });
-
-//   it('deve mostrar lista de vagas quando elas forem adicionadas', () => {
-//     const vagas: Vaga[] = [
-//       {
-//         nome: 'Vaga 1',
-//         salario: 1000,
-//         descricao: 'Descrição da vaga 1',
-//         empresa: 'Empresa 1',
-//         contato: 'Contato 1',
-//       },
-//       {
-//         nome: 'Vaga 2',
-//         salario: 2000,
-//         descricao: 'Descrição da vaga 2',
-//         empresa: 'Empresa 2',
-//         contato: 'Contato 2',
-//       },
-//     ];
-//     vagas.forEach((vaga) => servicoVagaService.adicionarVaga(vaga));
-//     fixture.detectChanges();
-
-//     const listaVagasElement: HTMLElement =
-//       fixture.nativeElement.querySelector('.tabela-vagas');
-//     expect(listaVagasElement).toBeTruthy();
-
-//     const linhas: NodeListOf<HTMLTableRowElement> =
-//       listaVagasElement.querySelectorAll('tr');
-
-//     // Iterar sobre cada linha
-//     linhas.forEach((linha, index) => {
-//       console.log('linha:', linha); // Verificar cada linha
-//       const listaItems: NodeListOf<HTMLTableCellElement> =
-//         linha.querySelectorAll('td tr');
-//       console.log('listaItems:', listaItems); // Verificar listaItems
-//       expect(listaItems[0].textContent).toContain(vagas[index].nome);
-//       expect(listaItems[1].textContent).toContain(vagas[index].descricao);
-//       expect(listaItems[2].textContent).toContain(vagas[index].empresa);
-//       expect(listaItems[3].textContent).toContain(vagas[index].contato);
-//       expect(listaItems[4].textContent).toContain(`R$ ${vagas[index].salario}`);
-//     });
-//   });
-// });
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabelaVagasComponent } from './tabela-vagas.component';
 import { ServicoVagaService, Vaga } from '../service/servico-vaga.service';
@@ -108,23 +43,21 @@ describe('TabelaVagasComponent', () => {
 
     const listaVagasElement: HTMLElement =
       fixture.nativeElement.querySelector('.tabela-vagas');
-    console.log('listaVagasElement:', listaVagasElement); 
     expect(listaVagasElement).toBeTruthy();
 
     const linhas: NodeListOf<HTMLTableRowElement> =
-      listaVagasElement.querySelectorAll('tr');
-    console.log('linhas:', linhas);
+      listaVagasElement.querySelectorAll('tbody > tr');
+
+    expect(linhas.length).toEqual(vagas.length);
     linhas.forEach((linha, index) => {
       const listaItems: NodeListOf<HTMLTableCellElement> =
         linha.querySelectorAll('td');
-      console.log('listaItems:', listaItems); 
+      expect(listaItems.length).toBeGreaterThan(0);
       expect(listaItems[0].textContent).toContain(vagas[index].nome);
       expect(listaItems[1].textContent).toContain(vagas[index].descricao);
       expect(listaItems[2].textContent).toContain(vagas[index].empresa);
       expect(listaItems[3].textContent).toContain(vagas[index].contato);
       expect(listaItems[4].textContent).toContain(`R$ ${vagas[index].salario}`);
-      
     });
   });
 });
-
