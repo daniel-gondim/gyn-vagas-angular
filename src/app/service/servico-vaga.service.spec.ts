eaad8f89f5816679409b48b752d1642829006b3f
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-14 22:41:55 -0300

    test: adiciona teste para verificar se alerta para cenário em que campo nome não foi preenchido

--- src/app/service/servico-vaga.service.spec.ts
+++ src/app/service/servico-vaga.service.spec.ts
@@ -10,17 +10,48 @@ describe('ServicoVagaService', () => {
     service = TestBed.inject(ServicoVagaService);
   });
 
-  it('should be created', () => {
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
   });
 
-  it('deve adicionar uma vaga', () => {
-    const vaga = { nome: 'Vaga 1', salario: 1000, descricao: 'Descrição da vaga 1', empresa: 'Empresa 1', contato: 'Contato 1' };
+  it('deve adicionar uma vaga se todos os campos estiverem preenchidos', () => {
+    const vaga = {
+      nome: 'Vaga 1',
+      salario: 1000,
+      descricao: 'Descrição da vaga 1',
+      empresa: 'Empresa 1',
+      contato: 'Contato 1',
+    };
 
     service.adicionarVaga(vaga);
     const vagas = service.obterVagas();
 
     expect(vagas.length).toBe(1);
     expect(vagas[0]).toEqual(vaga);
+  });
+
+  it('deve emitir um alerta caso o usuário deixe o campo Nome em branco', () => {
+    const alertSpy = spyOn(window, 'alert'); // Espionando a função de alerta global
+
+    const vaga = {
+      nome: '',
+      salario: 3000,
+      descricao: 'Vaga 1',
+      empresa: 'Empresa 1',
+      contato: 'Contato 1',
+    };
+
+    service.adicionarVaga(vaga);
+
+    // Verifica se o alerta foi chamado com a mensagem correta
+    expect(window.alert).toHaveBeenCalledWith(
+      'Por favor, preencha todos os campos.'
+    );
+
+    // Verifica se a vaga não foi adicionada
+    expect(service.adicionarVaga).not.toContain(vaga);
+
+    // Reinicia o espião para o próximo teste
+    // window.alert.calls.reset();
   });
 });
