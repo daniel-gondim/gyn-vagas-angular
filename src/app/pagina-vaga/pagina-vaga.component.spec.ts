0b6b63bf760f3179166b7d703f88f7a1f3cffe5d
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-17 20:35:31 -0300

    feat: commit inicial do componente pagina-vaga

---
+++ src/app/pagina-vaga/pagina-vaga.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaginaVagaComponent } from './pagina-vaga.component';
+
+describe('PaginaVagaComponent', () => {
+  let component: PaginaVagaComponent;
+  let fixture: ComponentFixture<PaginaVagaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PaginaVagaComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PaginaVagaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pagina-vaga/pagina-vaga.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-vaga',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-vaga.component.html',
+  styleUrl: './pagina-vaga.component.css'
+})
+export class PaginaVagaComponent {
+
+}
