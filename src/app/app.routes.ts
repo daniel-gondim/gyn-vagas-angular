9e7a05e1d21b098276aa450ab4d1ea70e10d6ac0
Edundes <100936984+Edundes@users.noreply.github.com>
2024-04-19 16:16:22 -0300

    Fix: Inclusão de rotas do cadastro e tabela de vagas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CadastroVagasComponent } from './cadastro-vagas/cadastro-vagas.component';
+import { TabelaVagasComponent } from './tabela-vagas/tabela-vagas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: 'cadastro-de-vagas',
+  component: CadastroVagasComponent,
+  title: 'Cadastro de Vagas'},
+
+  {path: 'tabela-de-vagas',
+  component: TabelaVagasComponent,
+  title: 'Tabela de Vagas'}
+
+];
