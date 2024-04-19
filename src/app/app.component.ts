0f639265f87605e7e9685ced565505498e2ffe27
Edundes <100936984+Edundes@users.noreply.github.com>
2024-04-19 16:17:18 -0300

    Fix: Inclusão do router outlet e imports dos routers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CadastroVagasComponent } from "./cadastro-vagas/cadastro-vagas.component";
 import { TabelaVagasComponent } from './tabela-vagas/tabela-vagas.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CadastroVagasComponent, TabelaVagasComponent]
+
+    imports: [RouterOutlet,
+      CadastroVagasComponent,
+      TabelaVagasComponent,
+      NavbarComponent,
+      RouterLink,
+      RouterLinkActive]
 })
 export class AppComponent {
   title = 'gyn-vagas-angular';
