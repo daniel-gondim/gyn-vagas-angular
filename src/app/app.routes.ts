ee37407e733a79c65f699fb8a9c4095f80764461
Edundes <100936984+Edundes@users.noreply.github.com>
2024-04-22 18:11:37 -0300

    Feature: Criação da pagina home e adição da rota dela ao navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CadastroVagasComponent } from './cadastro-vagas/cadastro-vagas.component';
 import { TabelaVagasComponent } from './tabela-vagas/tabela-vagas.component';
+import { PaginaHomeComponent } from './pagina-home/pagina-home.component';
 
 export const routes: Routes = [
 
@@ -10,6 +11,10 @@ export const routes: Routes = [
 
   {path: 'tabela-de-vagas',
   component: TabelaVagasComponent,
-  title: 'Tabela de Vagas'}
+  title: 'Tabela de Vagas'},
+
+  {path: 'home',
+  component: PaginaHomeComponent,
+  title: 'Home'}
 
 ];
---
+++ src/app/pagina-home/pagina-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-home.component.html',
+  styleUrl: './pagina-home.component.css'
+})
+export class PaginaHomeComponent {
+
+}
