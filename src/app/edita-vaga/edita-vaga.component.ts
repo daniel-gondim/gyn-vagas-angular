9725f77557287b5ebb26d4183f4a8f71f717ff67
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-25 16:12:40 -0300

    feat: adiciona componente editaVaga que será responsável pela lógica da funcionalidade de edição de Vaga

---
+++ src/app/edita-vaga/edita-vaga.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edita-vaga',
+  standalone: true,
+  imports: [],
+  templateUrl: './edita-vaga.component.html',
+  styleUrl: './edita-vaga.component.css'
+})
+export class EditaVagaComponent {
+
+}
