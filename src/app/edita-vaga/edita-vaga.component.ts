06726a3e84e8ffc4893b131478be99bc0a8085fa
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-05-26 15:12:46 -0300

    Adiciona componente EditaVaga para edição de vagas
    - Implementa o componente EditaVaga para permitir a edição de vagas.
    - Utiliza @Input para receber a vaga a ser editada.
    - Utiliza @Output para emitir eventos de cancelamento e edição concluída.
    - Adiciona métodos para salvar e cancelar edições de vagas.
    - Integração com o ServicoVagaService para persistência das alterações.

--- src/app/edita-vaga/edita-vaga.component.ts
+++ src/app/edita-vaga/edita-vaga.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {ServicoVagaService, Vaga} from "../service/servico-vaga.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-edita-vaga',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './edita-vaga.component.html',
   styleUrl: './edita-vaga.component.css'
 })
 export class EditaVagaComponent {
+  @Input() vaga: Vaga | null = null;
+  @Output() edicaoCancelada = new EventEmitter<void>();
+  @Output() vagaEditada = new EventEmitter<Vaga>();
 
+  constructor(private vagaService: ServicoVagaService) {}
+
+  salvarEdicao(): void {
+    if(this.vaga && this.vaga.id) {
+      this.vagaService.editarVaga(this.vaga.id.toString(), this.vaga).subscribe(
+        (vagaEditada) => {
+          this.vagaEditada.emit(vagaEditada);
+        },
+        (error) => {
+          alert('Erro ao editar vaga.');
+          console.log('Erro ao editar vaga', error)
+        }
+      )
+    }
+  }
+
+  cancelarEdicao() {
+
+  }
 }
