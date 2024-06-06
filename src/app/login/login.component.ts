dedac16b4511c36e22f3fca3a94b3b7e411bbda5
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-06-05 22:01:59 -0300

    feat: adicionar configuração de login e implementar authservice
    - adiciona as configurações necessárias para autenticação de usuários
    - implementa a classe AuthService para gerenciar a autenticação do usuário
    - configura rotas, serviços e componentes relacionados ao login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService} from "../service/auth.service";
+import {MatFormField} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormField
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login(username, password).subscribe(success => {
+        if (success) {
+          this.router.navigate(['/']);
+        } else {
+          // handle login failure
+        }
+      });
+    }
+  }
 }
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private loginUrl = 'http://localhost:8080/api/login'; // Substitua pela URL do seu endpoint de login
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post<any>(this.loginUrl, { username, password }).pipe(
+      map(response => {
+        if (response && response.token) {
+          localStorage.setItem('authToken', response.token);
+          return true;
+        }
+        return false;
+      }),
+      catchError(error => {
+        console.error(error);
+        return of(false);
+      })
+    );
+  }
+}
