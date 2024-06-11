b6f4f9b882c72438d2767ab3ebe2ee678a001581
Edundes <100936984+Edundes@users.noreply.github.com>
2024-06-11 11:28:49 -0300

    Fix: Removendo pasta src/assets/imagens/ do repositório e incluindo o componente rodapé em todas as páginas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,19 @@ import {CadastroVagasComponent} from "./cadastro-vagas/cadastro-vagas.component"
 import {TabelaVagasComponent} from './tabela-vagas/tabela-vagas.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {RouterOutlet} from "@angular/router";
+import { RodapeComponent } from "./rodape/rodape.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [HttpClientModule,
-    NavbarComponent,
-    RouterOutlet,
-    CadastroVagasComponent,
-    TabelaVagasComponent
-  ]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HttpClientModule,
+        NavbarComponent,
+        RouterOutlet,
+        CadastroVagasComponent,
+        TabelaVagasComponent, RodapeComponent]
 })
 
 export class AppComponent {
