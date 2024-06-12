66c6025d9597dd8e44a25cfa921758caff8fdc25
Edundes <100936984+Edundes@users.noreply.github.com>
2024-06-12 17:38:49 -0300

    Adiciona o path do cadastro e do perfil da empresa.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import {PaginaHomeComponent} from './pagina-home/pagina-home.component';
 import {EditaVagaComponent} from "./edita-vaga/edita-vaga.component";
 import {CurriculoComponent} from './curriculo/curriculo.component';
 import {PaginaBuscaVagasComponent} from "./pagina-busca-vagas/pagina-busca-vagas.component";
+import { PerfilEmpresaComponent } from './perfil-empresa/perfil-empresa.component';
+import { CadastroEmpresaComponent } from './cadastro-empresa/cadastro-empresa.component';
+
+
 
 export const routes: Routes = [
 
@@ -25,6 +29,19 @@ export const routes: Routes = [
     path: 'busca-de-vagas',
     component: PaginaBuscaVagasComponent,
     title: 'Busca de Vagas',
+  },
+
+  // Inclusão do path para a página de perfil da empresa
+  {
+    path: 'perfil-empresa',
+    component: PerfilEmpresaComponent,
+    title: 'Perfil Empresa',
+  },
+
+  {
+    path: 'cadastro-empresa',
+    component: CadastroEmpresaComponent,
+    title: 'Cadastro de Empresa'
   },
 
   {
